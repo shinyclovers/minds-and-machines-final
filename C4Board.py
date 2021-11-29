0cb82caccc1f3d7cb98825cc4df081b92adf357e
shinymegaaltaria <36224157+shinymegaaltaria@users.noreply.github.com>
2021-11-29 15:29:02 -0500

    prototype for score function and detectwinner function

--- C4Board.py
+++ C4Board.py
@@ -1,24 +1,28 @@
 
 class C4Board():
 	
-	def __init__(self,players = 2, height = 6, width = 7):
+	def __init__(self,players = 2, lineLength = 4, height = 6, width = 7):
 
 		# this board is column major
 		self.board = []
+		for i in range(width):
+			self.board.append([0] * height)
+		
 		self.height = height
 		self.width = width
 		self.maxPlayer = players
 		self.currentPlayer = 1
+		self.lineLength = lineLength
 
-		for i in range(width):
-			self.board.append([0] * height)
-		
 		# "height" of the tokens at the i-th column
-		self.height = [0] * width
+		self.heights = [0] * width
+
 	def updatePlayer(self):
-		pastPlayer = self.currentPlayer
+
 		self.currentPlayer+=1
-		# if self.currentPlayer > 1
+		if self.currentPlayer > self.maxPlayer:
+			self.currentPlayer = 1
+		
 	def addPiece(self,column,player):
 		
 		if self.height[column] >= 6:
@@ -26,20 +30,34 @@ class C4Board():
 		
 		else:
 			self.board[column][self.height[column]] = player
-			self.height[column] += 1
+			self.heights[column] += 1
 	
 	def detectLine(self):
+		dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+		dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 		for width in range(self.width):
 			for height in range(self.height):
-				# [width][height]
-				if not width < 3:
-					count = 0
-					for x in range(width):
-						if self.board[width][height] == self.currentPlayerplayer:
-							1
-					
-				
-				
+				# no player has placed a thing on this cell
+				if self.board[width][height] == 0:
+					continue
+				for i in range(8):
+					line_success = True
+					# check for line?
+	
+	def score(self):
+		score_table = []
+		for i in range(self.width):
+			score_table.append([0] * self.height)
 
+		dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+		dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+		for width in range(self.width):
+			for height in range(self.height):
+				for i in range(8):
+					# idk, do something here?
+					continue
 		
-		
+		print(score_table)
+
+a = C4Board()
+a.score()
